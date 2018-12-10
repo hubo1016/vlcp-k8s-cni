a5887f21eb5d89cc3d3b911bc112548cfe8d50e0
hubo <hubo1016@126.com>
2018-12-10 18:42:16 +0800

    build script

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe
+
+# Using docker
+
+mkdir -p dist
+
+# Build
+build(){
+	rm -rf build
+	mkdir -p build
+	cp -r Docker/$1/* build/
+	cp -r $1 build/
+	(cd build && docker build -t vlcp-k8s-tmp/$1 .)
+	docker run --rm vlcp-k8s-tmp/$1 tar c /$2 | tar x -C dist/
+}
+
+build vlcp-k8s-cni vlcp-k8s-cni
+build vlcp_k8s vlcp_k8s
